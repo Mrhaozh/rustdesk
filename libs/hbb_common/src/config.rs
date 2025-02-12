6e09ea266471418fdcdf4ce6d2d334e828c2f8f4
hzh <haozhihui@iyunting.com>
2025-02-12 10:21:47 +0800

    setting server

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -97,8 +97,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["172.16.0.186"];
+pub const RS_PUB_KEY: &str = "5MSB+Rs2tnSbplWZ3nYDp5O2dqvlhCnNaOJEt9oYDs4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
